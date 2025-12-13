b0a52ce2e040072f55332308673476b42ae996fe
Elena Belozerova <107627792+Pillow3003@users.noreply.github.com>
2025-12-13 18:48:37 +0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,15 @@
+class Registration:
+    user_name = 'User.'
+    password = "1234"
+    uncorrect_email = 'asd'
+    error = "Ошибка"
+    msg_autorization = 'Чтобы разместить объявление, авторизуйтесь'
+    error_class = 'input_inputError__fLUP9'
+
+class Urls:
+    url = "https://qa-desk.stand.praktikum-services.ru/"
+
+class Announcement:
+    name_announcement = "Котик"
+    description = 'В добрые руки'
+    price = '5000000'
